db9a1106cb1ec738fbe0117d1c212c5d847b60fe
Chetanananda Panigrahy <CPanigrahy@gep.com>
2020-04-22 13:27:12 +0530

    Test

--- ObjectComparer/ObjectComparer/Program.cs
+++ ObjectComparer/ObjectComparer/Program.cs
@@ -11,35 +11,6 @@ namespace Compare
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            TestClass invoice1 = new TestClass
-            {
-                MyProperty1 = 1,
-                MyProperty2 = 2,
-                TestClassChild = new TestClassChild
-                {
-                    MyProperty1 = 3
-                },
-                TestClassChildren = new List<TestClassChild>
-                {
-                    new TestClassChild{ MyProperty1 = 101},
-                    new TestClassChild{ MyProperty1 = 102},
-                }
-
-            };
-            TestClass invoice2 = new TestClass
-            {
-                MyProperty1 = 4,
-                MyProperty2 = 5,
-                TestClassChild = new TestClassChild
-                {
-                    MyProperty1 = 6
-                },
-                TestClassChildren = new List<TestClassChild>
-                {
-                    new TestClassChild{ MyProperty1 = 201},
-                    new TestClassChild{ MyProperty1 = 202},
-                }
-            };
             InvoiceSections invsec1 = JsonConvert.DeserializeObject<InvoiceSections>(File.ReadAllText(@"..\..\..\..\..\1"));
             InvoiceSections invsec2 = JsonConvert.DeserializeObject<InvoiceSections>(File.ReadAllText(@"..\..\..\..\..\2"));
             Console.WriteLine(JsonConvert.SerializeObject(new Comparer().Compare(invsec1, invsec2), Formatting.Indented));
@@ -47,16 +18,4 @@ namespace Compare
 
 
     }
-    class TestClass
-    {
-        public int MyProperty1 { get; set; }
-        public int MyProperty2 { get; set; }
-        public TestClassChild TestClassChild { get; set; }
-        public List<TestClassChild> TestClassChildren { get; set; }
-    }
-    class TestClassChild
-    {
-        public int MyProperty1 { get; set; }
-    }
-
 }
